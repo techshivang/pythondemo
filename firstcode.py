c9ad8939e5d7b65fa07711f78c21a8a0cbdbbba5
shivang saxena <1616510104@kit.ac.in>
2019-05-06 04:09:27 -0700

    just create a simple even program using list comprehension.

--- firstcode.py
+++ firstcode.py
@@ -4,3 +4,8 @@ print("Testing")
 print("Done")
 
 print("Some changes from telusko")
+print("hello navin its shivang")
+print("i will make a small program using list comprehension:")
+l=[1,2,3,4,5,6]
+even=[i for i in l if i%2==0]
+print(even)
